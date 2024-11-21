b42447e3feb07a7c3a88d73437eb5ee9eaa956b0
Michael Jay <mjay@bitpay.com>
2024-11-21 14:30:24 -0500

    refactor to add spl listener

--- backend/src/address-monitor/solana-address-monitor.service.ts
+++ backend/src/address-monitor/solana-address-monitor.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Connection, PublicKey } from '@solana/web3.js';
+import { Connection, ParsedInstruction, PublicKey } from '@solana/web3.js';
 import { InvoiceDbHandlerService } from 'src/invoice-db-handler/invoice-db-handler.service';
 import { LoggerService } from 'src/logger.service';
 
@@ -8,6 +8,7 @@ import { LoggerService } from 'src/logger.service';
 export class SolanaAddressMonitorService implements OnModuleInit {
   private connection: Connection;
   private acceptanceAccountPubKey: PublicKey;
+  private splAtaPubKey: PublicKey;
 
   constructor(
     private readonly configService: ConfigService,
@@ -46,6 +47,25 @@ export class SolanaAddressMonitorService implements OnModuleInit {
       );
       process.exit(1);
     }
+
+    const splAtaPubKey = this.configService.get<string>(
+      'SPL_ATA_PUBLIC_KEY_STRING',
+    );
+    if (!splAtaPubKey) {
+      console.error(
+        'SPL_ATA_PUBLIC_KEY_STRING is not defined in the environment variables - exiting process.',
+      );
+      process.exit(1);
+    }
+    try {
+      this.splAtaPubKey = new PublicKey(splAtaPubKey);
+    } catch (err) {
+      console.error(
+        'Invalid SPL_ATA_PUBLIC_KEY_STRING - exiting process.',
+        err.message,
+      );
+      process.exit(1);
+    }
   }
 
   async onModuleInit() {
@@ -66,57 +86,81 @@ export class SolanaAddressMonitorService implements OnModuleInit {
         console.log('System wallet account verified on-chain.');
       }
 
+      const onLogsHandler = async (logs: Logs, ctx: Context) => {
+        this.logger.log(logs, ctx);
+        const { signature } = logs;
+
+        let payerWallet: string | undefined = undefined;
+        let reference: string | undefined = undefined;
+
+        const tx = await this.connection.getParsedTransaction(signature);
+        if (!tx) throw new Error('Tx not found');
+        const { instructions } = tx.transaction.message;
+
+        const splMemoProgramId = 'MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr';
+        let memoInstruction = instructions.find((instruction) => {
+          const programId = instruction.programId.toString();
+          return programId === splMemoProgramId;
+        });
+        if (!memoInstruction) {
+          throw new Error("Can't associate transaction with invoice");
+        }
+        if (!(memoInstruction as ParsedInstruction).parsed) {
+          // Memo instruction is PartiallyDecodedInstruction - how do I parse it?
+          memoInstruction = memoInstruction as ParsedInstruction;
+        }
+        console.log(memoInstruction);
+        if (typeof (memoInstruction as ParsedInstruction).parsed !== 'string') {
+          // Log that the transaction can't be assocaited with an invoice because the memo instruction didn't have the expected form
+          throw new Error("Can't associate transaction with invoice");
+        }
+        reference = (memoInstruction as ParsedInstruction).parsed as string;
+
+        let transferInstruction = instructions.find(
+          (instruction) =>
+            (instruction as ParsedInstruction).parsed?.type === 'transfer',
+        );
+        if (!transferInstruction) {
+          throw new Error("Can't find transfer instruction");
+        }
+
+        if (!(transferInstruction as ParsedInstruction).parsed) {
+          // Transfer instruction is PartiallyDecodedInstruction - how do I parse it?
+          transferInstruction = transferInstruction;
+        }
+
+        const {
+          destination: _,
+          lamports: __,
+          source,
+        } = (transferInstruction as ParsedInstruction).parsed.info as {
+          destination: string;
+          lamports: number;
+          source: string;
+        };
+        payerWallet = source;
+
+        if (!reference) {
+          throw new Error('Reference not found - check log');
+        }
+        if (!payerWallet) {
+          throw new Error('Payer wallet not found - check log');
+        }
+
+        const invoice =
+          await this.invoiceDbHandler.getOneByReference(reference);
+
+        // For proof of concept, just assume that payment is always full
+        await this.invoiceDbHandler.update(invoice.id, {
+          status: 'PAID',
+          paidAt: new Date(),
+          payerWallet: payerWallet || 'not found',
+          signature,
+        });
+      };
       // Subscribe to logs
-      this.connection.onLogs(
-        this.acceptanceAccountPubKey,
-        async (logs, ctx) => {
-          this.logger.log(logs, ctx);
-          const { signature, logs: bcLogs } = logs;
-
-          let payerWallet: string | undefined = undefined;
-          let reference: string | undefined = undefined;
-          for (const log of bcLogs) {
-            if (log.includes('Program log: Signed by ')) {
-              payerWallet = log.split('Program log: Signed by ')[1];
-            } else if (log.includes('Program log: Memo')) {
-              const memoMatch = log.match(
-                /Program log: Memo \(len \d+\): "(.*)"/,
-              );
-              if (memoMatch && memoMatch[1]) {
-                reference = memoMatch[1];
-              }
-            }
-            if (payerWallet && reference) {
-              break;
-            }
-          }
-
-          if (!reference) {
-            throw new Error('Reference not found - check log');
-          }
-
-          if (!payerWallet) {
-            const tx = await this.connection.getParsedTransaction(signature);
-            const signer = tx?.transaction?.message?.accountKeys?.find(
-              (accountKey: any) => accountKey.signer,
-            );
-            if (signer) {
-              payerWallet = signer.pubkey.toString();
-            }
-          }
-
-          const invoice =
-            await this.invoiceDbHandler.getOneByReference(reference);
-
-          // For proof of concept, just assume that payment is always full
-          await this.invoiceDbHandler.update(invoice.id, {
-            status: 'PAID',
-            paidAt: new Date(),
-            payerWallet: payerWallet || 'not found',
-            signature,
-          });
-        },
-      );
+      this.connection.onLogs(this.acceptanceAccountPubKey, onLogsHandler);
+      this.connection.onLogs(this.splAtaPubKey, onLogsHandler);
     } catch (err) {
       console.error(
         'SolanaAddressMonitorService error - exiting process.',
